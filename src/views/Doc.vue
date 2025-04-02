aab847dc222c9e744bdc4cce75e10b1590dab212
Yujie Wu <allenwuyujie@gmail.com>
2025-04-02 21:48:21 +1100

    styling TopNav and Aside

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,23 @@
   <div>
     <TopNav />
     <div class="content">
-      <aside>sidebar</aside>
+      <aside>
+        <h2>Components</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs</router-link>
+          </li>
+        </ol>
+      </aside>
       <main>main content</main>
     </div>
   </div>
@@ -15,4 +31,22 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+aside {
+  background: lightblue;
+  width: 200px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
