c87cb2976b4781d0b5d1685d62cf58ede27a9aea
Yujie Wu <allenwuyujie@gmail.com>
2025-04-03 23:46:54 +1100

    create Button component

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>Button</div>
+  <h1>Button</h1>
+  <Button />
+  <Button theme="button"> BUTTON </Button>
+  <Button theme="link"> LINK </Button>
+  <Button theme="text"> TEXT </Button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Button from '../lib/Button.vue';
 
 export default defineComponent({
+  components: {
+    Button,
+  },
   setup() {
     return {};
   },
---
+++ src/lib/Button.vue
@@ -0,0 +1,62 @@
+<template>
+  <button
+    v-bind="$attrs"
+    class="aui-button"
+    :class="{ [`aui-button-${theme}`]: theme }"
+  >
+    <slot>Button</slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    theme: {
+      type: String,
+      default: 'button',
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss">
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.aui-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+}
+</style>
