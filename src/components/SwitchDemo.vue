b5462f160b08a75db54c50e1fc1dcef5b8de352b
Yujie Wu <allenwuyujie@gmail.com>
2025-04-03 22:44:41 +1100

    add Switch component

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>Switch</div>
+  <Switch v-model:value="isOn" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import Switch from '../lib/Switch.vue';
 
 export default defineComponent({
+  components: {
+    Switch: Switch,
+  },
   setup() {
-    return {};
+    const isOn = ref(true);
+
+    return { isOn };
   },
 });
 </script>
---
+++ src/lib/Switch.vue
@@ -0,0 +1,73 @@
+<template>
+  <button
+    @click="toggle"
+    :class="{ on: value }"
+  >
+    <span></span>
+  </button>
+  <div>{{ value }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit('update:value', !props.value);
+    };
+
+    return { toggle };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #bfbfbf;
+  border-radius: $h/2;
+  position: relative;
+
+  span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  }
+
+  &.on {
+    background: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+
+  &.on:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
+  }
+}
+</style>
