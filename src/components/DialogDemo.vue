397d00c708992c36e4e0152111dca878643fde71
Yujie Wu <allenwuyujie@gmail.com>
2025-04-05 00:42:12 +1100

    create Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>Dialog</div>
+  <div>
+    <h1>Dialog</h1>
+    <Button @click="toggle">toggle</Button>
+    <Dialog
+      v-model:visible="x"
+      :closeOnClickOverlay="false"
+      :ok="ok"
+      :cancel="cancel"
+    ></Dialog>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
 
 export default defineComponent({
+  components: {
+    Dialog,
+    Button,
+  },
   setup() {
-    return {};
+    const x = ref(false);
+
+    const toggle = () => {
+      x.value = !x.value;
+    };
+
+    const ok = () => {
+      return true;
+    };
+
+    const cancel = () => {};
+    return { x, toggle, ok, cancel };
   },
 });
 </script>
