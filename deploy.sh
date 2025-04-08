293e1eb427d67761050749e1b0dd205784d8efa2
Yujie Wu <allenwuyujie@gmail.com>
2025-04-08 23:19:06 +1000

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
-rm -rf dist
-p build
-cd dist
-git init
-git add .
-git commit -m 'deploy'
-git branch -M main
-git remote add origin git@github.com:allenwyj/allen-ui-website.git
-git push -f -u origin main
+rm -rf dist &&
+pnpm run build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m 'deploy' &&
+git branch -M main &&
+git remote add origin git@github.com:allenwyj/allen-ui-website.git &&
+git push -f -u origin main &&
 cd ..
+echo https://allenwyj.github.io/allen-ui-website/
