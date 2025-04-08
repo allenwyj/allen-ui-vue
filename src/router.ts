d15721c2aecae9487845a0634aa3844e727acc24
Yujie Wu <allenwuyujie@gmail.com>
2025-04-08 16:20:37 +1000

    use Markdown component instead of using vue files

--- src/router.ts
+++ src/router.ts
@@ -1,31 +1,41 @@
-import { createWebHashHistory, createRouter } from 'vue-router';
+import { h } from 'vue';
+
+import { createWebHashHistory, createRouter, RouteRecordRaw } from 'vue-router';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
 import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
-import DocDemo from './components/DocDemo.vue';
 import SwitchDemo from './components/SwitchDemo.vue';
+import Markdown from './components/Markdown.vue';
 
-import Intro from './views/Intro.vue';
-import Usage from './views/Usage.vue';
+import docmd from '../markdown/intro.md';
+import usagemd from '../markdown/get-started.md';
+
+const retrieveMarkdown = (content: string) =>
+  h(Markdown, { content, key: content });
+
 const history = createWebHashHistory();
+
+const routes: Array<RouteRecordRaw> = [
+  { path: '/', name: 'home', component: Home },
+  {
+    path: '/doc',
+    name: 'doc',
+    component: Doc,
+
+    children: [
+      { path: '', component: retrieveMarkdown(docmd) },
+      { path: 'get-started', component: retrieveMarkdown(usagemd) },
+      { path: 'switch', component: SwitchDemo },
+      { path: 'button', component: ButtonDemo },
+      { path: 'dialog', component: DialogDemo },
+      { path: 'tabs', component: TabsDemo },
+    ],
+  },
+];
+
 export const router = createRouter({
   history,
-  routes: [
-    { path: '/', component: Home },
-    {
-      path: '/doc',
-      component: Doc,
-
-      children: [
-        { path: '', component: Intro },
-        { path: 'get-started', component: Usage },
-        { path: 'switch', component: SwitchDemo },
-        { path: 'button', component: ButtonDemo },
-        { path: 'dialog', component: DialogDemo },
-        { path: 'tabs', component: TabsDemo },
-      ],
-    },
-  ],
+  routes,
 });
