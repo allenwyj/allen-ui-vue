c78d0169425ff64320340b1ec328eda61c2f9f33
Yujie Wu <allenwuyujie@gmail.com>
2025-04-08 16:19:10 +1000

    create Markdown component

---
+++ src/components/Markdown.vue
@@ -0,0 +1,18 @@
+<template>
+  <component
+    :is="content"
+    class="markdown-body"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
