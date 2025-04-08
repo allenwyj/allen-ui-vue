45afcd0f8b9aea89beedc975f7fcdade72a41ba6
Yujie Wu <allenwuyujie@gmail.com>
2025-04-08 15:52:03 +1000

    support md

---
+++ markdown/intro.md
@@ -0,0 +1,12 @@
+# Introduction
+
+Allen UI is a UI toolkit based on Vue 3 and TypeScript.
+
+The purpose of implementing this UI library is to learn more about Vue 3
+to expand my technical knowledge about how to build a component library
+and for some personal project use as well.
+
+So I strongly do not recommend you to use this UI library in production.
+Read source code for learning purpose.
+
+Next: [Installation](#/doc/get-started)
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,27 +1,18 @@
 <template>
   <article>
-    <h1>Introduction</h1>
-    <p>Allen UI is a UI toolkit based on Vue 3 and TypeScript.</p>
-    <p>
-      The purpose of implementing this UI library is to learn more about Vue 3
-      to expand my technical knowledge about how to build a component library
-      and for some personal project use as well.
-    </p>
-    <p>
-      So I strongly do not recommend you to use this UI library in production.
-      Read source code for learning purpose.
-    </p>
-    <p>Next: <router-link to="/doc/get-started">Installation</router-link></p>
+    <md />
   </article>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import md from '../../markdown/intro.md';
 
 export default defineComponent({
-  setup() {
-    return {};
+  components: {
+    md,
   },
+  setup() {},
 });
 </script>
 
